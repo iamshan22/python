f0f6004de7fc88f7dbf7d769b198cb7d8362c1fd
iamshan22 <konashankaraditya@gmail.com>
2024-03-15 12:09:52 +0530

    v1

---
+++ set/01.py
@@ -0,0 +1,39 @@
+#s1=set
+#print(s1)
+ 
+#s2= {10,20,30,40,50}
+#print(s2,type(s2))
+
+#s3={10,20,30,40,50,10,20,30,40}
+#print(s3,type(s3))
+
+
+#print(s2 + s3) TypeError: unsupported operand type(s) for +: 'set' and 'set'
+#print(s1+2) TypeError: unsupported operand type(s) for +: 'type' and 'int'
+
+
+#print(dir(set))
+
+#s1={10}
+#s1.add(20)
+#s1.add(30)
+#s1.add(40)
+#s1.add(50)
+#print(s1)
+
+#s1.clear()
+#print(s1)
+
+
+#s1= {10,20,30,40,50}
+#s1.remove(40)
+
+
+#s1= {10,20,30,40,50}
+#s1.discard(40)
+#print(s1)
+
+
+
+
+
