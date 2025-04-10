cdf8055c0f9275de0a506e6310f89633b80d38be
Kona Shankar Aditya <konashankaraditya@gmail.com>
2025-04-10 18:44:31 +0530

    v1

--- data_types/1.py
+++ data_types/1.py
@@ -1,3 +1,24 @@
 l5 =[10,20,30,40,50,10,10,40,60]
 print(l5[3:7:1]) 
 print(l5[1:8:3]) 
+
+
+# task 2 (Concatenating list objects):Create two lists and add it (control statement and function ,lambda function or advance function ,comprehension)
+l1 = [10,20,30,40,50]
+l2 = [60,70,80,90,100]
+print(l1+l2)# [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+# print(l1*l2) # TypeError: can't multiply sequence by non-int of type 'list'
+
+# task 3 : Repetition of list objects
+print(l1*3) # [10, 20, 30, 40, 50, 10, 20, 30, 40, 50, 10, 20, 30, 40, 50]
+
+# task 4 : packing and unpacking of list objects
+#a,b,c,d,e,f,g,h,i = [10,20,30,40,50,60,70,80,90]
+#print(a,b,d,e,g)
+
+
+a,b,*c,d = [10,20,30,40,50,60,70,80,90]
+print(a,b,d) # 10 20 90
+#ValueError: too many values to unpack (expected 4)
+
+
---
+++ data_types/2.py
@@ -0,0 +1,18 @@
+# Theory: Tuple
+# Tuple : Tuple is safe
+# In  Tuple we can pass duplicate Elements,and it maintains insertion  elements
+# Tuple is immutable
+# we can create tuple by using paranthesis ````() and object tuple()````
+
+# Example 1: create tuple
+t1 = 10
+print(t1 ,type(t1)) # <class 'int'>
+
+t2 = 10,
+print(t2 ,type(t2)) # <class 'tuple'>
+
+t3 = 10,20,30
+print(t3 ,type(t3)) # <class 'tuple'>
+
+t4 = (10,20,30)
+print(t4 ,type(t4)) # <class 'tuple'>
